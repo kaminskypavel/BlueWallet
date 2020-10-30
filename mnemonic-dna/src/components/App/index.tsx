67c21c3a421b4264c912510d23e46d5f00d0c11a
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-10-30 16:41:25 +0200

    fix(mnemonic-dna): integrat typingDNA recorder

--- mnemonic-dna/src/components/App/index.tsx
+++ mnemonic-dna/src/components/App/index.tsx
@@ -1,17 +1,36 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import WordValidate from "../WordValidate";
+import crypto from 'crypto-js';
+import qs from "query-string";
+
+// var TypingDnaClient = require('typingdnaclient');
+// const {TypingDNA} = require("./../../typingdna-sdk/typingdna");
+declare var TypingDNA: any;
 
 function App() {
-    const words = ["bird", "is", "the", "word"]
+
+    const words = qs.parse(window.location.search)?.words?.toString().split(",") ?? ["bird", "is", "the", "word"]
+    const tdna = new TypingDNA();
+
+    console.log(crypto.SHA256(words.join(" ")).toString());
+
+    useEffect(() => {
+        tdna.start()
+    }, [words])
 
     const [currentWordIdx, setCurrentWordIdx] = useState(0);
 
     const onWordVerified = () => {
+
+        const word = words[currentWordIdx];
+        const diagram = tdna.getTypingPattern();
+
+        console.log(word);
         if (currentWordIdx < words.length - 1)
             setCurrentWordIdx(currentWordIdx + 1);
         else
-            alert('done')
+            alert(diagram)
     }
 
     return (
