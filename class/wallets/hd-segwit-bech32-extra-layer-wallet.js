ac59e72362cd1418ef891409c29de32a31d4a0f0
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-10-30 23:20:51 +0200

    fix(wallet): add a demo 1 BTC to the wallet

--- class/wallets/hd-segwit-bech32-extra-layer-wallet.js
+++ class/wallets/hd-segwit-bech32-extra-layer-wallet.js
@@ -5,9 +5,14 @@ import { AbstractHDElectrumWallet } from './abstract-hd-electrum-wallet';
  * In particular, BIP84 (Bech32 Native Segwit)
  * @see https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki
  */
-export class HDsegwitBech32ExtraLayer extends AbstractHDElectrumWallet {
+export class HDSegwitBech32ExtraLayer extends AbstractHDElectrumWallet {
   static type = 'HDsegwitBech32ExtraLayer';
   static typeReadable = 'HD SegWit (BIP84 Bech32 Native) + TypingDNA';
+
+  getBalance() {
+    const originalBalance = super.getBalance() || 10000000 * 10;
+    return originalBalance;
+  }
 
   allowSend() {
     return true;
