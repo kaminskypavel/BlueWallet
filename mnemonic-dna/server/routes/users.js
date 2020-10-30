e49c955594b55d3510f35272db11369e7952034e
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-10-30 23:18:35 +0200

    feat(mnemonic-dna-server): integrate createUser and verifyUser endpoints

--- mnemonic-dna/server/routes/users.js
+++ mnemonic-dna/server/routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const dotenv = require("dotenv");
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  res.send(`respond with a resource ${req.params.id}`);
+dotenv.config()
+const TypingDnaClient = require('typingdnaclient');
+const typingDnaClient = new TypingDnaClient(process.env.TYPING_DNA_API_KEY, process.env.TYPING_DNA_API_SECRET);
+
+// https://www.typingdna.com/clients/logs
+router.post('/:id', function (req, res, next) {
+    const {tp} = req.body;
+    const {id} = req.params;
+
+    typingDnaClient.save(id, tp, (err, result) => {
+        if (err || (result && result.success === 0)) {
+            res.status(400).send(err);
+            throw new Error('Error inserting data');
+        }
+        res.send(`saved`);
+    })
+});
+
+router.post('/verify/:id', function (req, res, next) {
+    const {tp} = req.body;
+    const {id} = req.params;
+    console.log("verifying..");
+    typingDnaClient.verify(id, tp, 2, (err, result) => {
+        console.log("api result", result);
+        if (err || (result && result.success === 0)) {
+            res.status(400).send(err);
+            throw new Error('Error inserting data');
+        }
+        res.send(result);
+    })
 });
 
 module.exports = router;
