eeaabe0581bbfd45e8faf98229b1ee10d10638ab
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-10-31 11:08:09 +0200

    feat(mnemonic-dna): add routing by applying ?type=record/verify to the querystring

--- mnemonic-dna/src/components/App/index.tsx
+++ mnemonic-dna/src/components/App/index.tsx
@@ -1,18 +1,22 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import qs from "query-string";
-import {TypingDNAContext} from "../../context";
 import {MnemonicRecorder} from "../MnemonicRecorder";
+import {MnemonicVerifier} from '../MnemonicVerifier';
 
-
+// routing is done by applying ?type=record/verify to the querystring
 const App = () => {
     const queryStringObj = qs.parse(window.location.search, {arrayFormat: "comma"});
     const type = queryStringObj.type ?? "record";
-
+    const words = queryStringObj?.words?.toString().split(",") ?? ["bird", "is", "the", "word"]
+    console.log(type,words);
     switch (type.toString().toLowerCase()) {
+        case "verify" :
+            return <MnemonicVerifier words={words}/>
+            break;
+
         default:
         case "record" :
-            const words = queryStringObj?.words?.toString().split(",") ?? ["bird", "is", "the", "word"]
             return <MnemonicRecorder words={words}/>
             break;
 
