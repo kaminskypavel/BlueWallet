9cc4f372d1048bca697466e5d2de1f6e0ab11cb0
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-10-31 14:49:56 +0200

    chore: add real device to proxy.bat

--- proxy.bat
+++ proxy.bat
@@ -1,2 +1,6 @@
 adb -s emulator-5554 reverse tcp:8081 tcp:8081
 adb -s emulator-5554 reverse tcp:3000 tcp:3000
+
+REM if you're using a real device change its ID here
+adb -s 9A251FFBA007TM reverse tcp:8081 tcp:8081
+adb -s 9A251FFBA007TM reverse tcp:3000 tcp:3000
