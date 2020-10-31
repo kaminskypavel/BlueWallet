3fda7a72bd3d6a274f2c20dbb64dfe367009cac5
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-10-31 10:56:40 +0200

    feat(mnemonic-dna): move TypingDNA sdk to context so we can use it via hooks

---
+++ mnemonic-dna/src/context/index.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+const tdna = new TypingDNA();
+
+export const TypingDNAContext = React.createContext(tdna);
--- mnemonic-dna/src/index.tsx
+++ mnemonic-dna/src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {TypingDNAContext} from "./context";
+
+const tdna = new TypingDNA();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <TypingDNAContext.Provider value={tdna}>
+            <App/>
+        </TypingDNAContext.Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
